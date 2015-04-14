0260899e07bfc53f0cda470a574ffe6403544ab3
PhilipShilling <philipshilling@bootpanel.net>
2015-04-13 18:25:21 -0700

    Fixed method not exists error

--- src/redstonelamp/plugin/PluginDescription.java
+++ src/redstonelamp/plugin/PluginDescription.java
@@ -1,9 +1,15 @@
 package redstonelamp.plugin;
 
+import java.io.InputStream;
+
 public class PluginDescription {
 	private String name;
 	
 	public String getName() {
 		return name;
+	}
+
+	public void setStream(InputStream is) {
+		//TODO
 	}
 }
