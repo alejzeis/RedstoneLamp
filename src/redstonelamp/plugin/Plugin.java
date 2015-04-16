19e498e6e29281e204cd4d8c8448c9b59bbfb16b
unnivm <unnivm@gmail.com>
2015-04-17 00:03:29 +0530

    added plug-in life cycle methods

--- src/redstonelamp/plugin/Plugin.java
+++ src/redstonelamp/plugin/Plugin.java
@@ -1,28 +1,37 @@
+
 package redstonelamp.plugin;
 
 import java.util.ArrayList;
 
 import redstonelamp.Server;
+import redstonelamp.command.CommandExecutor;
 import redstonelamp.logger.Logger;
 
-public interface Plugin {
+public interface Plugin extends CommandExecutor {
+
 	public PluginDescription getDescription();
-	
+
 	public String getDataFolder();
-	
+
 	public String getResource(String file);
-	
+
 	public ArrayList<String> getResources();
-	
+
 	public Server getServer();
-	
+
 	public String getName();
-	
+
 	public Logger getLogger();
-	
+
 	public PluginLoader getPluginLoader();
-	
+
 	public boolean isEnabled();
-	
+
 	public boolean isDisabled();
+
+	public void onLoad();
+
+	public void onEnable();
+
+	public void onDisable();
 }
