f228c7fbf0c74550da3c82dc71fb25151a5bb012
Jayden Mak <hoyinm.dev@gmail.com>
2015-04-18 12:04:57 +0800

    Health manage

--- src/redstonelamp/Player.java
+++ src/redstonelamp/Player.java
@@ -8,4 +8,31 @@ public class Player {
 	public String getName() {
 		return null;
 	}
+	
+	public void setHealth(int health){
+		if(health > this.getMaxHealth()){
+			return false;
+		}
+		if(health =< 0){
+			this.kill();
+			return false;
+		}
+		//Send player health packet
+		//TODO
+	}
+	
+	public int getMaxHealth(){
+		return this.maxhealth;
+	}
+	
+	public void kill(){
+		//Send player kill packet
+		this.sendMessage("You died.");
+	}
+	
+	public int getHealth(){
+		return this.health;
+	}
+	
+	
 }
