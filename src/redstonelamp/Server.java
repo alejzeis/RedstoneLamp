ee4fccdcca131222843d1144b614305feb3ea497
jython234 <jython234@hotmail.com>
2015-07-14 17:10:03 -0500

    Move non-network code from JRakLibInterface to Server

--- src/redstonelamp/Server.java
+++ src/redstonelamp/Server.java
@@ -31,11 +31,18 @@ public class Server implements Runnable{
         debugMode = Boolean.parseBoolean(properties.getProperty("debug", "false"));
         bindInterface = properties.getProperty("server-ip", "0.0.0.0");
         bindPort = Integer.parseInt(properties.getProperty("port", "19132"));
+
+        logger.info("This server is running " + RedstoneLamp.SOFTWARE + " version " + RedstoneLamp.VERSION + " \"" + RedstoneLamp.CODENAME + "\" (API " + RedstoneLamp.API_VERSION + ")");
+        logger.info(RedstoneLamp.SOFTWARE + " is distributed under the " + RedstoneLamp.LICENSE);
+        
         network = new Network(this);
         network.registerInterface(new JRakLibInterface(this));
 
         RedstoneLamp.setServerInstance(this);
         pluginManager = new PluginManager();
+
+        pluginManager.getPluginLoader().loadPlugins();
+        logger.info("Done! Type \"help\" or \"?\" for help.");
         
         running = true;
         run();
--- src/redstonelamp/network/JRakLibInterface.java
+++ src/redstonelamp/network/JRakLibInterface.java
@@ -29,18 +29,18 @@ public class JRakLibInterface implements ServerInstance, NetworkInterface{
     public JRakLibInterface(Server server){
         this.server = server;
         server.getLogger().info("Starting Minecraft: PE server v" + NetworkInfo.MCPE_VERSION + " (protocol " + NetworkInfo.MCPE_PROTOCOL + ")");
+
         rakLibLogger = new JRakLibLogger();
         rakLib = new JRakLibServer(rakLibLogger, server.getBindPort(), server.getBindInterface());
         exceptionHandler = new JRakLibExceptionHandler();
         rakLib.setUncaughtExceptionHandler(exceptionHandler);
         interface_ = new ServerHandler(rakLib, this);
+
         setName(server.getProperties().getProperty("motd"));
+
         server.getLogger().info("Server running on " + server.getBindInterface() + ":" + server.getBindPort());
-        server.getLogger().info("This server is running " + RedstoneLamp.SOFTWARE + " version " + RedstoneLamp.VERSION + " \""+ RedstoneLamp.CODENAME +"\" (API " + RedstoneLamp.API_VERSION + ")");
-        server.getLogger().info(RedstoneLamp.SOFTWARE + " is distributed under the " + RedstoneLamp.LICENSE);
+
         interface_.sendOption("portChecking", "false");
-        server.getPluginManager().getPluginLoader().loadPlugins();
-        server.getLogger().info("Done! Type \"help\" or \"?\" for help.");
     }
 
     @Override
