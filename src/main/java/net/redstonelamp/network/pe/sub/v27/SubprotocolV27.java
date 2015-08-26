9c1e8c819246f810ba18abf2e5cbed70c1a1d4d1
jython234 <jython234@hotmail.com>
2015-08-26 11:12:14 -0500

    Sync changes

--- src/main/java/net/redstonelamp/network/pe/sub/v27/SubprotocolV27.java
+++ src/main/java/net/redstonelamp/network/pe/sub/v27/SubprotocolV27.java
@@ -61,7 +61,9 @@ public class SubprotocolV27 extends Subprotocol implements ProtocolConst27{
                 up.bb().skip(8); //Skip protocol1, protocol 2 (int, int)
                 lr.clientId = up.bb().getInt();
                 lr.slim = up.bb().getByte() > 0;
-                lr.skin = up.bb().get(up.bb().getUnsignedShort()); //Skin written as a String, but the String class seems to corrupt the Skin
+                //lr.skin = up.bb().get(up.bb().getUnsignedShort()); //Skin written as a String, but the String class seems to corrupt the Skin
+                up.bb().skip(2);
+                lr.skin = up.bb().get(up.bb().remaining());
 
                 requests.add(lr);
                 break;
@@ -227,12 +229,14 @@ public class SubprotocolV27 extends Subprotocol implements ProtocolConst27{
             bb.putInt(flags);
             packets.add(new UniversalPacket(bb.toArray(), ByteOrder.BIG_ENDIAN, address));
 
+            /*
             byte[] metadata = player.getMetadata().toBytes();
             bb = BinaryBuffer.newInstance(9 + metadata.length, ByteOrder.BIG_ENDIAN);
             bb.putByte(SET_ENTITY_DATA_PACKET);
             bb.putLong(0); //Player Entity ID is always zero to themselves
             bb.put(metadata);
             packets.add(new UniversalPacket(bb.toArray(), ByteOrder.BIG_ENDIAN, address));
+            */
 
             bb = BinaryBuffer.newInstance(6, ByteOrder.BIG_ENDIAN);
             bb.putByte(SET_TIME_PACKET);
@@ -281,8 +285,8 @@ public class SubprotocolV27 extends Subprotocol implements ProtocolConst27{
         } else if(response instanceof AddPlayerResponse) {
             Player p = ((AddPlayerResponse) response).player;
             byte[] meta = p.getMetadata().toBytes();
-            //bb = BinaryBuffer.newInstance(56 + p.getSkin().length + p.getNametag().getBytes().length + meta.length, ByteOrder.BIG_ENDIAN);
-            bb = BinaryBuffer.newInstance(0, ByteOrder.BIG_ENDIAN);
+            bb = BinaryBuffer.newInstance(56 + p.getSkin().length + p.getNametag().getBytes().length + meta.length, ByteOrder.BIG_ENDIAN);
+            //bb = BinaryBuffer.newInstance(0, ByteOrder.BIG_ENDIAN);
             bb.putByte(ADD_PLAYER_PACKET);
             bb.putLong(p.getEntityID()); //Prevent client from knowing the real clientID
             bb.putString(p.getNametag());
@@ -295,6 +299,7 @@ public class SubprotocolV27 extends Subprotocol implements ProtocolConst27{
             bb.putFloat(0f); //speed z
             bb.putFloat(p.getPosition().getYaw());
             bb.putFloat(p.getPosition().getYaw()); //TODO: head yaw
+            bb.putFloat(p.getPosition().getPitch());
             bb.putShort((short) 0);
             bb.putShort((short) 0);
             bb.putByte((byte) (p.isSlim() ? 1 : 0));
