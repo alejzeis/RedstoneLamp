038b2130208b0ab11dd7f1bfde668a5aeef4cf74
Trent <trent0308@live.com>
2015-08-27 19:08:14 -0500

    Added finish message to fixmaven.bat

--- fixmaven.bat
+++ fixmaven.bat
@@ -1,3 +1,6 @@
 :: This simple batch file is to fix eclipse dependency errors, it downloads
 :: The maven dependencies and adds them to the classpath for eclipse
+
 mvn eclipse:eclipse
+echo "Finished refresh of depencies!"
+pause
