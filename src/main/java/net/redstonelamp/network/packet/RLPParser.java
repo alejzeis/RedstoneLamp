94987037dde672627eae3d6c90dda56bb0434684
Trent <trent0308@live.com>
2015-10-05 22:24:31 -0500

    Revert "Revert "Added HeaderChecker""
    This reverts commit ace389724257579a73a38e51d1fb77af299a2126.

--- src/main/java/net/redstonelamp/network/packet/RLPParser.java
+++ src/main/java/net/redstonelamp/network/packet/RLPParser.java
@@ -1,5 +1,3 @@
-package net.redstonelamp.network.packet;
-
 /*
  * This file is part of RedstoneLamp.
  *
@@ -16,6 +14,7 @@ package net.redstonelamp.network.packet;
  * You should have received a copy of the GNU Lesser General Public License
  * along with RedstoneLamp.  If not, see <http://www.gnu.org/licenses/>.
  */
+package net.redstonelamp.network.packet;
 
 import net.redstonelamp.request.Request;
 
